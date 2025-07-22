114fcac0fd831f8cd35308f2e1e1d06ae754add9
Lukasz Szaszkiewicz <lukasz.szaszkiewicz@gmail.com>
2025-07-22 13:04:20 +0200

    ./hack/update-codegen.sh

--- staging/src/k8s.io/client-go/kubernetes/fake/clientset_generated.go
+++ staging/src/k8s.io/client-go/kubernetes/fake/clientset_generated.go
@@ -21,6 +21,7 @@ package fake
 import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/watch"
 	applyconfigurations "k8s.io/client-go/applyconfigurations"
 	"k8s.io/client-go/discovery"
@@ -157,13 +158,15 @@ func NewSimpleClientset(objects ...runtime.Object) *Clientset {
 	cs.discovery = &fakediscovery.FakeDiscovery{Fake: &cs.Fake}
 	cs.AddReactor("*", "*", testing.ObjectReaction(o))
 	cs.AddWatchReactor("*", func(action testing.Action) (handled bool, ret watch.Interface, err error) {
+		var gvk schema.GroupVersionKind
 		var opts metav1.ListOptions
-		if watchActcion, ok := action.(testing.WatchActionImpl); ok {
-			opts = watchActcion.ListOptions
+		if watchAction, ok := action.(testing.WatchActionImpl); ok {
+			gvk = watchAction.Kind
+			opts = watchAction.ListOptions
 		}
 		gvr := action.GetResource()
 		ns := action.GetNamespace()
-		watch, err := o.Watch(gvr, ns, opts)
+		watch, err := o.Watch(gvr, gvk, ns, opts)
 		if err != nil {
 			return false, nil, err
 		}
@@ -211,12 +214,14 @@ func NewClientset(objects ...runtime.Object) *Clientset {
 	cs.AddReactor("*", "*", testing.ObjectReaction(o))
 	cs.AddWatchReactor("*", func(action testing.Action) (handled bool, ret watch.Interface, err error) {
 		var opts metav1.ListOptions
+		var gvk schema.GroupVersionKind
 		if watchAction, ok := action.(testing.WatchActionImpl); ok {
+			gvk = watchAction.Kind
 			opts = watchAction.ListOptions
 		}
 		gvr := action.GetResource()
 		ns := action.GetNamespace()
-		watch, err := o.Watch(gvr, ns, opts)
+		watch, err := o.Watch(gvr, gvk, ns, opts)
 		if err != nil {
 			return false, nil, err
 		}
